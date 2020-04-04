41eec4d72d2c48ce126415e18d9cd36a9e1f9649
WildeMedia <wildemedia33@gmail.com>
2020-04-04 15:47:28 +0100

    Added sample.txt to project and used the fetch api to output it to the console.

--- api.js
+++ api.js
@@ -1,6 +1,11 @@
 document.getElementById('getText').addEventListener
 ('click', getText);
 
-function getText(){
-  console.log(123);
+function getText() {
+  fetch('sample.txt')
+  .then(res =>  res.text())
+  .then(data => {
+    
+    console.log(data);
+  })
 }
